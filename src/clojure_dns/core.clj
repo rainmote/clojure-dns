aabaefb945cc92b19cab85f74be414adfb22a1b9
rainmote <rainmote@gmail.com>
2018-08-25 14:27:26 +0800

    init repo

---
+++ src/clojure_dns/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-dns.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_dns/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-dns.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-dns.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
